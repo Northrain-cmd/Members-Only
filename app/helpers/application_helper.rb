f022c238baaebaf26816dfbdb1a708788d3b9948
Northrain-cmd <northrain@icloud.com>
2021-05-14 14:02:00 +0300

    Login and logout added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    bootstrap_alert_class = {
+      'success' => 'alert-success',
+      'error' => 'alert-danger',
+      'notice' => 'alert-info',
+      'alert' => 'alert-danger',
+      'warn' => 'alert-warning'
+    }
+    bootstrap_alert_class[type]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  before_create do
+    generate_token(:auth_token)
+  end
   has_secure_password
   validates :email, :username, presence: true
+
+  private
+
+  def generate_token(col)
+    loop do
+      self[col] = SecureRandom.urlsafe_base64
+      break unless User.exists?(col => self[col])
+    end
+  end
 end
