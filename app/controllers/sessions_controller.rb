d48a85f4031f46bbaf0a24db4f1758d1ff08bbd1
Northrain-cmd <northrain@icloud.com>
2021-05-15 16:39:26 +0300

    Added remember_me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      cookies[:auth_token] = user.auth_token
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       redirect_to root_url, notice: 'Logged in!'
     else
       flash.now.alert = 'Email or password invalid'
