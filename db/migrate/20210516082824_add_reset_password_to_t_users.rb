ae51305c3040f963e7c2ca9b307e09a97e7f9614
Northrain-cmd <northrain@icloud.com>
2021-05-22 13:14:47 +0300

    Migration fixed

--- db/migrate/20210516082824_add_reset_password_to_t_users.rb
+++ db/migrate/20210516082824_add_reset_password_to_t_users.rb
@@ -2,6 +2,6 @@ class AddResetPasswordToTUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :reset_password_token, :string
     add_column :users, :password_reset_sent_at, :time
+    add_index :users, :reset_password_token
   end
-  add_index :users, :reset_password_token
 end
